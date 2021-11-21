a8382a06975d89946f96b60b35c9d2e24d7e9d38
Akashkapase9988 <akashkapase9988@gmail.com>
2021-11-21 13:16:49 +0530

    [akash added code for roll dice]

--- 085-SnakeLadder/Program.cs
+++ 085-SnakeLadder/Program.cs
@@ -7,7 +7,12 @@ namespace _085_SnakeLadder
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to snake ladder Game");
-            
+            int Start_Position = 0;
+            Random random = new Random();
+
+            int Dice_No = random.Next(1, 7);
+            Console.WriteLine("Player roll dice and dice no is :  " + "   " +Dice_No);
+
         }
     }
 }
