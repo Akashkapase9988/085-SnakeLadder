ff4767b394daef38dcd7533621beb609d04c1141
Akashkapase9988 <akashkapase9988@gmail.com>
2021-11-21 13:08:09 +0530

    Add project files.

---
+++ 085-SnakeLadder/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace _085_SnakeLadder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to snake ladder Game");
+            
+        }
+    }
+}
